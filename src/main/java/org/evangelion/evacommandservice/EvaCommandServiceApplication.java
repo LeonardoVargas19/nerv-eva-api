c1c1bf37ecced7c4a9172457ff2c461642ee86dc
leoromero <leonardo.vargas.ulsa@gmail.com>
2025-04-13 20:46:09 -0600

    First commit

---
+++ src/main/java/org/evangelion/evacommandservice/EvaCommandServiceApplication.java
@@ -0,0 +1,13 @@
+package org.evangelion.evacommandservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaCommandServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvaCommandServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/evangelion/evacommandservice/EvaCommandServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.evangelion.evacommandservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaCommandServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
